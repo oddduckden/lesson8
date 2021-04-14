a11b46338ff6f2451580002d93c0a8ff017a0a31
oddduckden <dkireev.71@gmail.com>
2021-04-14 22:13:07 +0300

    task6

--- task6.py
+++ task6.py
@@ -12,6 +12,7 @@ class Warehouse:
         self.__items = {Printer: [], Scanner: [], Xerox: []}
         self.__vocub = {'Printer': Printer, 'Scanner': Scanner, 'Xerox': Xerox}
         self.__distribute = {Printer: [], Scanner: [], Xerox: []}
+        self.__overload = []
 
     def add(self, item):
         if sum([len(self.__items[x]) for x in self.__items.keys()]) <= self.__maxnumber:
@@ -21,6 +22,7 @@ class Warehouse:
                     print(f'Поступил на склад: {item}')
         else:
             print('на складе нет места')
+            self.__overload.append(item)
 
     def distribute(self, item, department, value):
         __clss = self.__vocub[item]
@@ -30,8 +32,13 @@ class Warehouse:
                 self.__distribute[__clss].append(el)
                 el.department = department
                 print(f'Выдан: {el} в {el.department}')
+                w.check_overload()
                 return
         print('Данная позиция на складе отсутствует')
+
+    def check_overload(self):
+        if self.__overload:
+            w.add(self.__overload.pop(0))
 
 
 class OfficeEquipment:
@@ -80,17 +87,23 @@ class Xerox(OfficeEquipment):
 ddct = {'Printer': Printer, 'Scanner': Scanner, 'Xerox': Xerox}
 add_list = [('Printer', 'HP LaserJet', 300, 'B&W', 2), ('Scanner', 'Canon lide 400', 100, False, 1),
             ('Xerox', 'Kyocera 3551', 500, 'double-side', 1), ('Printer', 'HP Inkjet', 100, 'Color', 1),
-            ('Printer', 'HP laserJet', 300, 'Color', 2), ('Printer', 'Canon ip6500', 400, 'Color', 1)]
+            ('Printer', 'HP laserJet', 300, 'B&W', 2), ('Printer', 'Canon ip6500', 400, 'Color', 1)]
 w = Warehouse()
 for i in add_list:
     for n in range(i[-1]):
         c = ddct[i[0]](i[1], i[2], i[3])
         w.add(c)
-print('Заказан: Принтер для бухгалтерии, черно-белый. ', end='')
+print('Заказан: Принтер в бухгалтерию, черно-белый. ', end='')
 w.distribute('Printer', 'accounts', 'B&W')
-print('Заказан: Принтер для маркетинга, цветной. ', end='')
+print('Заказан: Принтер в маркетинг, цветной. ', end='')
 w.distribute('Printer', 'marketing', 'Color')
+print('Заказан: Принтер в отдел разработки, цветной. ', end='')
+w.distribute('Printer', 'R&D', 'Color')
 print('Заказан: Сканер для директора. ', end='')
 w.distribute('Scanner', 'CEO', False)
-print('Заказан: Сканер для бухгалтерии. ', end='')
+# c = Printer('Canon ip6500', 400, 'Color')
+# w.add(c)
+print('Заказан: Сканер в бухгалтерию. ', end='')
 w.distribute('Scanner', 'accounts', False)
+print('Заказан: Принтер в отдел разработи, цветной. ', end='')
+w.distribute('Printer', 'R&D', 'Color')
